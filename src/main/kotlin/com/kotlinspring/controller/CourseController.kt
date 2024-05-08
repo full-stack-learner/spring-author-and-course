59ca8e103918127c83c4743578c430c4a8313009
Zaw Moe Htike <uchihamoee@gmail.com>
2024-05-08 16:06:58 +0700

    first

---
+++ src/main/kotlin/com/kotlinspring/controller/CourseController.kt
@@ -0,0 +1,33 @@
+package com.kotlinspring.controller
+
+import com.kotlinspring.dto.CourseDTO
+import com.kotlinspring.service.CourseService
+import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/v1/courses")
+@Validated
+class CourseController(val courseService : CourseService) {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun addCourse(@RequestBody courseDTO: CourseDTO): CourseDTO {
+        return courseService.addCourse(courseDTO)
+    }
+
+    @GetMapping
+    fun retrieveAllCourses() : List<CourseDTO> = courseService.retrieveAllCourses()
+
+    //courseId
+    @PutMapping("/{course_id}")
+    fun updateCourse(@RequestBody courseDTO: CourseDTO
+                     , @PathVariable("course_id") courseId : Int)
+    = courseService.updateCourse(courseId, courseDTO)
+
+    @DeleteMapping("/{course_id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteCourse(@PathVariable("course_id") courseId : Int)
+    = courseService.deleteCourse(courseId)
+}
